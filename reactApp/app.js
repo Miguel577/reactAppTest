b0d3d6e499ee59274d23246f09190f78a09223a3
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-07-19 16:22:24 -0700

    todo and todolist static components

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,9 +1,60 @@
 import React  from 'react';
 import ReactDOM from 'react-dom';
 
+const dummyData = ["eat chipotle", "cry", "marry Lisa", "code??", "watch GOT", "shower"]
 
+//individual todo item
+class Todo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <li><button>X</button>  {this.props.task}</li>
+      </div>
+    )
+  }
+}
 
+//box around assortment of todos
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+        <ul>
+          {dummyData.map((item) => <Todo task={item}>{item}</Todo>)
+        }
+        </ul>
+    )
+  }
+}
 
+//input field and submit button to add new todos
+// class InputLine extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
 
-ReactDOM.render(<p>React lives!</p>,
+//big box lots of stuff in it
+// class TodoApp extends React.Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render() {
+//     return (
+//
+//     )
+//   }
+// }
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
