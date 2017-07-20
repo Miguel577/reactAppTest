1964bd2ba6e70433ecb0d977ece526ebe6ddad33
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-07-19 17:02:27 -0700

    added state and componentDidMount to TodoApp

--- reactApp/app.js
+++ reactApp/app.js
@@ -32,7 +32,7 @@ class TodoList extends React.Component {
   render() {
     return (
         <ul>
-          {dummyData.map((item) =>
+          {this.props.todos.map((item) =>
             item.completed ? (
               <Todo task={<strike>{item.taskText}</strike>}></Todo>
             ) : (
@@ -64,12 +64,20 @@ class InputLine extends React.Component {
 class TodoApp extends React.Component {
   constructor(props) {
     super(props)
+    this.state={
+      todos: [],
+    }
   }
+
+  componentDidMount() {
+    this.setState({todos:dummyData})
+  }
+
   render() {
     return (
       <div>
         <InputLine/>
-        <TodoList/>
+        <TodoList todos={this.state.todos}/>
       </div>
     )
   }
